8b1b6981150d5f1c981ffb0a7f6b3374b9c786d9
Vera Kvisgaard <verakvisgaard@gmail.com>
2022-11-29 19:09:20 +0100

    Update factor_product.R

--- R/factor_product.R
+++ R/factor_product.R
@@ -1,11 +1,15 @@
 
 
+
+
 #' Title
 #'
 #' @param x,y arrays. Factors to be multiplied. One dimension for each variable
 #'    in scope. Dimensions must be named (to find common variables).
 #'
 #' @return an array with the factor product of x and y
+#' @export
+#'
 factor_product <- function(x, y){
 
   # find common vars
@@ -18,29 +22,27 @@ factor_product <- function(x, y){
   } else {
 
     vars  <- union(vars_x, vars_y)
-    cards <- c(dim(x), dim(y)[!vars_y %in% sepset])
+    cards <- setNames(c(dim(x), dim(y)[!vars_y %in% sepset]), vars)
 
-    stride_x <- stride_y <- cards*0
-    stride_x[vars%in%vars_x] <- c(1, cumprod(dim(x)[-length(vars_x)]))
-    stride_y[vars%in%vars_y] <- c(1, cumprod(dim(y)[-length(vars_y)]))
-
+    stride_x <- setNames(c(1, cumprod(dim(x)[-length(vars_x)])), vars_x)
+    stride_y <- setNames(c(1, cumprod(dim(y)[-length(vars_y)])), vars_y)
 
     j <- 0
     k <- 0
-    assignment <- 0*cards
+    assignment <- cards*0
     out <- array(dim = cards,
                  dimnames = c(dimnames(x), dimnames(y)[!vars_y %in% sepset]))
     for (i in seq_along(out)){
       out[i] <- x[j+1]*y[k+1]
-      for (l in seq_along(cards)){
-        assignment[l] <- assignment[l]+1
-        if (assignment[l] == cards[l]){
-          assignment[l] <- 0
-          j <- j - (cards[l]-1)*stride_x[l]
-          k <- k - (cards[l]-1)*stride_y[l]
+      for (v in vars){
+        assignment[v] <- assignment[v]+1
+        if (assignment[v] == cards[v]){
+          assignment[v] <- 0
+          if (any(v == vars_x)) j <- j - (cards[v]-1)*stride_x[v]
+          if (any(v == vars_y)) k <- k - (cards[v]-1)*stride_y[v]
         } else {
-          j <- j + stride_x[l]
-          k <- k + stride_y[l]
+          if (any(v == vars_x)) j <- j + stride_x[v]
+          if (any(v == vars_y)) k <- k + stride_y[v]
           break
         }
       }
@@ -48,4 +50,3 @@ factor_product <- function(x, y){
     return(out)
   }
 }
-

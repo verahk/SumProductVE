a219315ffbd67b48ddf9982be91162fb06818be3
Vera Kvisgaard <verahk@math.uio.no>
2022-11-29 18:43:29 +0100

    add tests

--- tests/testthat/test-factor_product.R
+++ tests/testthat/test-factor_product.R
@@ -12,4 +12,20 @@ test_that("function returns correct product", {
 
   expect_equal(factor_product(px, pyz.x), p)
   expect_equal(factor_product(pyz.x, px), p)
+
+  N <- 10^3
+  data  <- sapply(levels, sample, size  = N, replace = TRUE)
+  count <- table(as.data.frame(data))
+  p     <- count/N
+
+  px    <- array(rowSums(count)/N, k[1], levels[1])
+  py.x  <- sweep(rowSums(count, dims = 2), 1, rowSums(count), "/")
+  pz.xy <- sweep(count, 1:2, rowSums(count, dims = 2), "/")
+
+  expect_equal(factor_product(px, py.x), rowSums(count, dims = 2)/N, ignore_attr = TRUE)
+  expect_equal(factor_product(factor_product(px, py.x), pz.xy), count/N, ignore_attr = TRUE)
+  expect_equal(Reduce(factor_product, list(px, py.x, pz.xy)), count/N, ignore_attr = TRUE)
+  expect_equal(Reduce(factor_product, rev(list(px, py.x, pz.xy))), count/N, ignore_attr = TRUE)
+  expect_equal(Reduce(factor_product, rev(list(py.x, px, pz.xy))), count/N, ignore_attr = TRUE)
 })
+

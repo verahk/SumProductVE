134fb145a161da79d5c88558fb62f536522debe7
Vera Kvisgaard <verahk@math.uio.no>
2022-11-29 16:26:30 +0100

    Fix errors due to namechange of package

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(SumProduct)
+library(SumProductVE)
 
-test_check("SumProduct")
+test_check("SumProductVE")
